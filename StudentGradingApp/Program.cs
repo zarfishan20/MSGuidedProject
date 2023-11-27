ed4bd9899cd0537a591b70bb140015fa242c431e
zfali <zfali@DESKTOP-M56GFNV>
2023-11-27 12:26:46 +0000

    update

--- StudentGradingApp/Program.cs
+++ StudentGradingApp/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+        int[] sophia = { 90, 86, 87, 98, 100 };
+
+
+        int sophiaSum = 1;
+
+
+        foreach (int sum in sophia)
+
+        {
+            Console.WriteLine(sum);
+            sophiaSum += sophia[sum];
+
+        }
+
+        Console.WriteLine(sophiaSum);
+ 
+
+
+
+
+
+
+
+
+
+
+
+ 
